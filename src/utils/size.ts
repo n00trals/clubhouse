071b6de824faac6995d5df00491604bd3043ecb1
n00trals <n00trals@phork.org>
2022-11-21 11:49:06 -0800

    :lipstick: feat: updated font, loading animation

--- src/utils/size.ts
+++ src/utils/size.ts
@@ -5,16 +5,11 @@ import {
   PAPER_SIDE_OFFSET,
   SMALL_PAPER_TOP_OFFSET,
   SMALL_PAPER_SIDE_OFFSET,
-  MIN_GRID_WIDTH,
 } from 'config/sizes';
 import { viewports } from 'config/viewports';
 
 export const showMobileLayout = (width = 0): boolean => width <= viewports.small.max;
 export const showDesktopLayout = (width = 0): boolean => width >= viewports.medium.min;
-
-export const showFullWidthCards = (width = 0): boolean => width <= MIN_GRID_WIDTH;
-export const showFullPageCards = (width = 0): boolean => width >= viewports.xsmall.min;
-export const showVerticalPage = showMobileLayout;
 
 export const getPaperTopOffset = (width = 0): number =>
   width <= viewports.small.max ? SMALL_PAPER_TOP_OFFSET : PAPER_TOP_OFFSET;
